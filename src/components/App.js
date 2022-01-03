0148dd435ecc02afe2ede37282cfed5531a01cf7
mohahdy <mohahdyc@hotmail.com>
2022-01-04 01:36:53 +0200

    Implemented the getusers action and reducer

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
+import {connect} from 'react-redux';
+import React,{ useEffect } from 'react';
+import handleGetUsers from '../actions/users'
+function App(props) {
+  useEffect(() =>{
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+  props.dispatch(handleGetUsers())},[])
 
-function App() {
   return (
     <div >
       <header >
-       Header
+        Users List
       </header>
-      This is Would you Rather App?
+      <ul>
+      {props.users.map((user)=>(        
+        <li key={user}>
+          {user}
+        </li>
+      ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
+function mapStateToProps({users}) {
+  return {users: Object.keys(users)}
+}
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import {Provider} from 'react-redux';
+import {createStore} from 'redux'
+import reducer from './reducers'
+import middleware from './middleware'
+const store = createStore(reducer, middleware);
+class ProviderApp extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+          <App />
+      </Provider>
+    )
+  }
+}ReactDOM.render(
+    <ProviderApp />,
   document.getElementById('root')
 );
 
